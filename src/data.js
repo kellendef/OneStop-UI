4c8578908b18d819660d84a39f4f77433453c143
Kellen Fairchild <fairchildkellend@gmail.com>
2022-03-04 21:59:43 -0800

    Deleted old files remaining from 'create-react-app'

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        'Name': 'Cheeseburger',
+        'Description': 'A cheeseburger with all the fixins\'.',
+        'Price': 5.0,
+        'id': 1
+    },
+    {
+        'Name': 'Bacon Cheeseburger',
+        'Description': 'A cheeseburger with all the fixins\' and bacon.',
+        'Price': 6.0,
+        'id': 2
+    },
+    {
+        'Name': 'Gyro',
+        'Description': 'A classic Greek favorite with Gyro meat, yogurt sauce, letuce, onions, and tomatoes on pita bread.',
+        'Price': 6.5,
+        'id': 3
+    }
+]
+
+export default data;
